d4eabaeb5d85e77c136eef1aefa0b1af38f69006
diffy <hello@different-name.dev>
2026-02-09 12:37:24 +1000

    refactor(dyad/nixos/substituters): use flake nixConfig for subtituter config

--- dyad/nixos/nix/substituters.nix
+++ dyad/nixos/nix/substituters.nix
@@ -1,25 +1,19 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  self,
+  ...
+}:
+let
+  inherit (import "${self}/flake.nix") nixConfig;
+in
 {
   options.dyad.nix.substituters.enable = lib.mkEnableOption "substitutors config";
 
   config = lib.mkIf config.dyad.nix.substituters.enable {
     nix.settings = {
-      substituters = [
-        # high priority since it's almost always used
-        "https://cache.nixos.org?priority=10"
-
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://cache.nixos-cuda.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-      ];
+      substituters = nixConfig.trusted-substituters;
+      inherit (nixConfig) trusted-public-keys;
     };
   };
 }
