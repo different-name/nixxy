5d158972a4fb2932012e358b7a50a3e461918e0c
diffy <hello@different-name.dev>
2026-02-10 21:43:11 +1000

    chore(sources): update, remove winetricks

--- dyad/nixos/programs/steam.nix
+++ dyad/nixos/programs/steam.nix
@@ -2,7 +2,6 @@
   lib,
   config,
   inputs',
-  self',
   pkgs,
   ...
 }:
@@ -14,18 +13,7 @@
       enable = true;
 
       gamescopeSession.enable = true;
-
-      protontricks = {
-        enable = true;
-        package = pkgs.protontricks.override {
-          # newer revision with https://github.com/Winetricks/winetricks/pull/2318
-          # fixes https://github.com/Matoking/protontricks/issues/286
-          # can be removed once there's a new revision of winetricks
-          winetricks = pkgs.winetricks.overrideAttrs {
-            inherit (self'.sources.winetricks) src version;
-          };
-        };
-      };
+      protontricks.enable = true;
 
       extraCompatPackages = with pkgs; [
         inputs'.nixpkgs-xr.packages.proton-ge-rtsp-bin
