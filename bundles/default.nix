019a8479f1afc92ffbd78380477077a7837b4b6f
diffy <hello@different-name.dev>
2026-02-17 15:41:06 +1000

    refactor(bundles/default.nix): tidy up

--- bundles/default.nix
+++ bundles/default.nix
@@ -1,41 +1,25 @@
 { lib, inputs, ... }:
 let
-  x86_64-linux = {
-    system = "x86_64-linux";
-    class = "nixos";
-  };
+  diffyHosts = [
+    "sodium"
+    "potassium"
+    "iodine"
+    "chinchilla"
+  ];
 in
 {
   bundle = {
-    hosts = {
-      # keep-sorted start
-      chinchilla = x86_64-linux;
-      iodine = x86_64-linux;
-      potassium = x86_64-linux;
-      sodium = x86_64-linux;
-      # keep-sorted end
-    };
+    hosts = lib.genAttrs diffyHosts (_: {
+      system = "x86_64-linux";
+      class = "nixos";
+    });
 
     users.diffy = {
-      shared.imports = [
-        (inputs.import-tree ./users/diffy/shared)
-      ];
+      shared.imports = lib.singleton (inputs.import-tree ./users/diffy/shared);
 
-      hosts =
-        lib.genAttrs
-          [
-            # keep-sorted start
-            "chinchilla"
-            "iodine"
-            "potassium"
-            "sodium"
-            # keep-sorted end
-          ]
-          (host: {
-            imports = [
-              (inputs.import-tree ./users/diffy/hosts/${host})
-            ];
-          });
+      hosts = lib.genAttrs diffyHosts (host: {
+        imports = lib.singleton (inputs.import-tree ./users/diffy/hosts/${host});
+      });
     };
   };
 }
