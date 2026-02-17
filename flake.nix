7809bf8cdc43877379eca2cf63e89e69dd801c7d
diffy <hello@different-name.dev>
2026-02-18 08:39:37 +1000

    revert(flake.lock): update

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
     };
 
     # hyprland wayland compositor
-    hyprland.url = "github:hyprwm/Hyprland/f68ac7ef7589e1536d438f7fbfb3ad987538fe0f";
+    hyprland.url = "github:hyprwm/Hyprland";
 
     # manage persistent state
     impermanence = {
