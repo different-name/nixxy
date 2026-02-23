eafe8a5b01a4cd8dc09d1ec5df182b4c69786714
diffy <hello@different-name.dev>
2026-02-23 14:11:41 +1000

    fix(desktop/hyprpaper): disable splash

--- bundles/users/diffy/shared/dyad/desktop/hyprpaper/default.nix
+++ bundles/users/diffy/shared/dyad/desktop/hyprpaper/default.nix
@@ -3,9 +3,13 @@ bundleLib.mkEnableModule [ "dyad" "desktop" "hyprpaper" ] {
   home-manager.services.hyprpaper = {
     enable = true;
 
-    settings.wallpaper = lib.singleton {
-      monitor = "";
-      path = toString ./wallpaper.jpg;
+    settings = {
+      wallpaper = lib.singleton {
+        monitor = "";
+        path = toString ./wallpaper.jpg;
+      };
+
+      splash = false;
     };
   };
 }
