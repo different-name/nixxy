d2fb910e806d9795c6014f4a3fd6ebf57e491af7
diffy <hello@different-name.dev>
2026-04-23 01:27:59 +1000

    fix(terminal/shell-aliases): use librewolf cookies for yt-dlp alias

--- diffy/shared/terminal/shell-aliases.nix
+++ diffy/shared/terminal/shell-aliases.nix
@@ -1,6 +1,6 @@
 { bundleLib, ... }:
 bundleLib.mkEnableModule [ "dyad" "terminal" "shellAliases" ] {
   home-manager.home.shellAliases = {
-    ytdl = "yt-dlp -t mp4 --no-playlist";
+    ytdl = ''yt-dlp -t mp4 --no-playlist --cookies-from-browser "firefox:~/.librewolf"'';
   };
 }
