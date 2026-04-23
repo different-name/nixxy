8583562359f4d4c54723181e9dce4ab952a39f24
diffy <hello@different-name.dev>
2026-04-23 11:58:25 +1000

    fix(applications/obs-studio): disable nvidia explicit sync

--- diffy/shared/applications/obs-studio.nix
+++ diffy/shared/applications/obs-studio.nix
@@ -1,15 +1,34 @@
-{ bundleLib, ... }:
+{
+  bundleLib,
+  inputs,
+  ...
+}:
+let
+  mkWrappedObs =
+    pkgs:
+    inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.obs-studio;
+      env.__NV_DISABLE_EXPLICIT_SYNC = 1;
+    };
+in
 bundleLib.mkEnableModule [ "dyad" "applications" "obs-studio" ] {
-  nixos.programs.obs-studio = {
-    enable = true;
-    enableVirtualCamera = true;
-  };
+  nixos =
+    { pkgs, ... }:
+    {
+      programs.obs-studio = {
+        enable = true;
+        package = mkWrappedObs pkgs;
+        enableVirtualCamera = true;
+      };
+    };
 
   home-manager =
     { pkgs, ... }:
     {
       programs.obs-studio = {
         enable = true;
+        package = mkWrappedObs pkgs;
         plugins = [
           pkgs.obs-studio-plugins.obs-move-transition
         ];
