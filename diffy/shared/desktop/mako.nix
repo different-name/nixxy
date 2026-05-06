0703999933b6f77a0d12de4810dd59e38ae7ce43
diffy <hello@different-name.dev>
2026-05-06 17:57:24 +1000

    chore(desktop/mako): reduce dnd notification time

--- diffy/shared/desktop/mako.nix
+++ diffy/shared/desktop/mako.nix
@@ -35,10 +35,10 @@ bundleLib.mkEnableModule [ "dyad" "desktop" "mako" ] {
           text = ''
             if makoctl mode | grep -q '\<do-not-disturb\>'; then
                 makoctl mode -r do-not-disturb
-                hyprctl notify -1 5000 "rgb(${borderColor})" "Notifications Shown"
+                hyprctl notify -1 1000 "rgb(${borderColor})" "Notifications Shown"
             else
                 makoctl mode -a do-not-disturb
-                hyprctl notify -1 5000 "rgb(${borderColor})" "Notifications Hidden"
+                hyprctl notify -1 1000 "rgb(${borderColor})" "Notifications Hidden"
             fi
           '';
         })
