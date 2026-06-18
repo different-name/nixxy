e9127c03eb5b73a42e856211afa1531cf4e67c28
diffy <hello@different-name.dev>
2026-06-18 21:26:56 +1000

    feat(hosts/sodium)!: remove postgresql

--- diffy/hosts/sodium/default.nix
+++ diffy/hosts/sodium/default.nix
@@ -34,68 +34,55 @@ in
     media.goxlr-utility.enable = true;
   };
 
-  nixos =
-    { pkgs, ... }:
-    {
-      imports = [
-        # keep-sorted start
-        (inputs.import-tree ./_nixos)
-        inputs.nixos-hardware.nixosModules.common-cpu-amd
-        inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-        inputs.nixos-hardware.nixosModules.common-pc-ssd
-        self.nixosModules.tty1Autologin
-        # keep-sorted end
-      ];
+  nixos = {
+    imports = [
+      # keep-sorted start
+      (inputs.import-tree ./_nixos)
+      inputs.nixos-hardware.nixosModules.common-cpu-amd
+      inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+      inputs.nixos-hardware.nixosModules.common-pc-ssd
+      self.nixosModules.tty1Autologin
+      # keep-sorted end
+    ];
 
-      system.stateVersion = "26.05";
+    system.stateVersion = "26.05";
 
-      networking = {
-        hostName = "sodium";
-        hostId = lib.substring 0 8 machineId;
-      };
-
-      environment.etc.machine-id.text = machineId;
-
-      services = {
-        tty1Autologin = {
-          enable = true;
-          user = "diffy";
-        };
-
-        goxlr-utility.enable = true;
-
-        postgresql = {
-          enable = true;
-          package = pkgs.postgresql_17;
-
-          authentication = pkgs.lib.mkOverride 10 ''
-            #type database DBuser origin-address auth-method
-            local all      all     trust
-            host  all      all     127.0.0.1/32   trust
-          '';
-        };
-      };
-
-      environment.sessionVariables = {
-        STEAM_FORCE_DESKTOPUI_SCALING = "1.5";
-        GDK_SCALE = "2";
-      };
-
-      programs.steam.gamescopeSession.args = [
-        "-W"
-        "3840"
-        "-H"
-        "2160"
-        "-r"
-        "144"
-        "-f"
-        "--hdr-enabled"
-        "--prefer-output"
-        "HDMI-A-1"
-      ];
-
-      hardware.brillo.enable = true; # backlight control
+    networking = {
+      hostName = "sodium";
+      hostId = lib.substring 0 8 machineId;
     };
+
+    environment.etc.machine-id.text = machineId;
+
+    services = {
+      tty1Autologin = {
+        enable = true;
+        user = "diffy";
+      };
+
+      goxlr-utility.enable = true;
+    };
+
+    environment.sessionVariables = {
+      STEAM_FORCE_DESKTOPUI_SCALING = "1.5";
+      GDK_SCALE = "2";
+    };
+
+    programs.steam.gamescopeSession.args = [
+      "-W"
+      "3840"
+      "-H"
+      "2160"
+      "-r"
+      "144"
+      "-f"
+      "--hdr-enabled"
+      "--prefer-output"
+      "HDMI-A-1"
+    ];
+
+    hardware.brillo.enable = true; # backlight control
+  };
 
   home-manager = {
     programs.btop.settings.cpu_sensor = "k10temp/Tctl";
