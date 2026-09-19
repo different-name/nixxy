c1a606f389b77c276c2ba4d96352011a07357c57
diffy <hello@different-name.dev>
2026-09-19 10:24:39 +1000

    chore(games/steam): remove unplayed game config

--- diffy/shared/games/steam/default.nix
+++ diffy/shared/games/steam/default.nix
@@ -37,36 +37,6 @@ bundleLib.mkEnableModule [ "dyad" "games" "steam" ] {
 
       defaultCompatTool = pkgs.proton-ge-bin;
       desktopEntries.enable = true;
-
-      apps = {
-        # keep-sorted start block=yes newline_separated=yes
-        "1091500" = {
-          name = "Cyberpunk 2077";
-          compatTool = pkgs.proton-ge-bin;
-
-          dllOverrides = {
-            winmm = "n,b";
-            version = "n,b";
-          };
-          args = [
-            "--launcher-skip"
-            "-skipStartScreen"
-          ];
-        };
-
-        "1361210" = {
-          name = "Warhammer 40k Darktide";
-          compatTool = pkgs.proton-ge-bin;
-
-          env.LD_PRELOAD = null;
-          preHook = ''
-            for i in "''${!game_command[@]}"; do
-              game_command[i]="''${game_command[i]//\/launcher\/Launcher.exe/\/binaries\/Darktide.exe}"
-            done
-          '';
-        };
-        # keep-sorted end
-      };
     };
 
     systemd.user.services.steam-silent = {
